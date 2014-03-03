76c631bbeb51cd402d6e6ba49cb224028957bfb7
Michał Parniak <michal.parniak@student.uw.edu.pl>
2014-03-03 18:50:24 +0100

    minor upgrades

--- tcspc.py
+++ tcspc.py
@@ -39,14 +39,34 @@ E=np.array(E)
 irfX=np.array(irfX)
 irfY=np.array(irfY)
 
+print 'give number of exponents:'
+numexp=int(sys.stdin.readline())
 
+print 'function to fit: f(t)='
+for i in range(numexp):
+    print 'A_'+str(i) +' exp(-t / tau_'+str(i)+') +'
+print 'B'
 
-def residuals(p,Y,X,E,irf):
-    model=p[1]*np.exp((-1)*X/p[0])+p[2]
-    return (np.fft.irfft(np.fft.rfft(irf)*np.fft.rfft(model))-Y)/E
-    
-p0=[3500.0,1000.0,1.0]
-fit=leastsq(residuals,p0,args=(Y,X,E,irfY),full_output=True)
+p0=[]
+print 'initial conditions:'
+for i in range(numexp):
+	print 'tau_'+str(i)
+	p0.append(float(sys.stdin.readline()))
+	print 'A_'+str(i)
+	p0.append(float(sys.stdin.readline()))
+print 'B'
+p0.append(float(sys.stdin.readline()))
+
+def model(p,X,numexp):
+    ret=p[2*(numexp-1)+2]
+    for i in xrange(numexp):
+        ret+=p[2*i+1]*np.exp((-1)*X/p[2*i])
+    return ret
+        
+def residuals(p,Y,X,E,irf,numexp):
+    return (np.fft.irfft(np.fft.rfft(irf)*np.fft.rfft(model(p,X,numexp)))/len(Y)-Y)/E
+
+fit=leastsq(residuals,p0,args=(Y,X,E,irfY,numexp),full_output=True)
 if(fit[4]<=4 and fit[4] >=1):
     print 'fit succesful!'
     print 'converged in '+str(fit[2]['nfev'])+' steps'
@@ -57,14 +77,16 @@ else:
 
 cov = fit[1]
 p=fit[0]
-fitres_model=p[1]*np.exp((-1)*X/p[0])+p[2]
-fitres=np.fft.irfft(np.fft.rfft(irfY)*np.fft.rfft(fitres_model))
+fitres_model=model(p,X,numexp)
+fitres=np.fft.irfft(np.fft.rfft(irfY)*np.fft.rfft(fitres_model))/len(Y)
 s_sq = (fit[2]['fvec']**2).sum()/(len(Y)-len(p))
-i_sq = (residuals(p0,Y,X,E,irfY)**2).sum()/(len(Y)-len(p))
+i_sq = (residuals(p0,Y,X,E,irfY,numexp)**2).sum()/(len(Y)-len(p))
 print 'reduced chi squared = '+str(s_sq)
 print 'reduced chi squared at initial point = '+str(i_sq)
-print 'tau='+str(round(p[0],1))+' +/- '+str(round(s_sq*cov[0][0],1))+' ps'
-
+for i in range(numexp):
+    print 'tau_'+str(i)+'='+str(round(p[2*i],1))+' +/- '+str(round(s_sq*cov[2*i][2*i],1))+' ps'
+    print 'A_'+str(i)+'='+str(p[2*i+1])+' +/- '+str(s_sq*cov[2*i+1][2*i+1])
+print 'B='+str(p[2*numexp])+' +/- '+str(s_sq*cov[2*numexp][2*numexp])
 
 f=open(sys.argv[2][:-4]+'_results.txt','w')
 i=0
@@ -84,5 +106,8 @@ plt.plot(X,fitres)
 plt.xlabel('Time [ps]')
 plt.ylabel('Counts')
 plt.title('Fluorescence decay fit results for '+sys.argv[2]+' and '+ sys.argv[1])
-plt.legend(['Experimental Data','IRF',r'Fit: $ \tau ='+str(round(p[0],1))+'\pm'+str(round(s_sq*cov[0][0],1))+'$ ps'])
+legendstring='Fit:\n'
+for i in range(numexp):
+    legendstring+=r'$\tau_'+str(i)+' ='+str(round(p[2*i],1))+'\pm'+str(round(s_sq*cov[2*i][2*i],1))+'$ ps\n'
+plt.legend(['Experimental Data','IRF',legendstring])
 plt.show()
